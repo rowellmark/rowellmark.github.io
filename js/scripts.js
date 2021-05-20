e388ddacd50a8220b4c74d9ef9956c3ab654d2b8
rowell.blanca <teksnip101@gmail.com>
2021-05-20 10:40:44 +0800

    Added new banner

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,58 @@
 
 			this.luxy();
 			this.go_to();
-			this.particle();
+			// this.particle();
 			this.skill_bar();
 			this.banner_adjust();
 			this.banner_typer();
 			this.fixed_header();
 
 		
+			const hero = document.querySelector('[data-hero]')
+
+			/* Menu */
+			const menuButton = document.querySelector('[data-btn="menu"]')
+			const menu = document.querySelector('[data-menu]')
+
+			menuButton.addEventListener('click', () => {
+				menu.classList.toggle('is-open')
+				menuButton.classList.toggle('is-active')
+			})
+
+			/* Timeline */
+			const tl = gsap.timeline()
+
+			tl
+				.to(hero, {
+					'--maskSize1': '20%',
+					duration: 0.5,
+				ease: 'back.out(2)',
+				})
+				.to(hero, {
+					'--maskSize2': '28%',
+				'--maskSize3': 'calc(28% + 0.1rem)',
+					duration: 0.5,
+				delay: 0.5,
+				ease: 'back.out(2)',
+				})
+
+			/* Cursor */
+			window.addEventListener('mousemove', (e) => {
+				const { clientX, clientY } = e
+			const x = Math.round((clientX / window.innerWidth) * 100)
+			const y = Math.round((clientY / window.innerHeight) * 100)
+				
+				gsap.to(hero, {
+				'--x': `${x}%`,
+				'--y': `${y}%`,
+				duration: 0.3,
+				ease: 'sine.out',
+			})
+			})
 
 		},
 		luxy : function(){
-			luxy.init({
-				wrapper: '#mainwrapper',
-				targets : '.luxy-el',
-				wrapperSpeed:  0.08
-			});
+
 
 		},
 		go_to : function(){
@@ -33,11 +70,19 @@
 			      $('html,body').animate({
 			        scrollTop: target.offset().top - 75
 			      }, 1000);
+
+	
 			      return false;
 			    }
 			  }
+
+
 			 });
 
+			 jQuery('.menu a').on('click', function(){
+				jQuery('.menu-button').trigger('click');
+			 });
+			 
 		},
 		fixed_header: function(){
 
